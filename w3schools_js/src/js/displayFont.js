970bf493fe37036bcc89dfec22c386403a3db540
jihyunKwon <jihyunkwon29@gmail.com>
2019-12-22 19:23:21 +0900

    Add 'script tag locations'

---
+++ w3schools_js/src/js/displayFont.js
@@ -0,0 +1,7 @@
+function displayFont(){
+    if(document.getElementById('demo2').style.display==='none'){
+        document.getElementById('demo2').style.display='block';
+    } else {
+        document.getElementById('demo2').style.display='none';
+    }
+}
