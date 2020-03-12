0df5e4fcd1e245f846da9c3297225b432eedf61f
jihyun.kwon <jihyunkwon29@gmail.com>
2020-03-12 10:58:50 +0900

    ssh 키 등록 테스트

--- mdn_js/src/200311_some.js
+++ mdn_js/src/200311_some.js
@@ -1,6 +1,6 @@
 const array = [1,2,3,4,5];
 //some() 메서드는 배열 안의 어떤 요소라도 주어진 판별 함수를 통과하는지 테스트합니다
-//빈배열은 무조건 false 반환 
+//빈배열은 무조건 false 반환
 
 const even = (element) => element % 2 === 0;
 console.log(array.some(even));
