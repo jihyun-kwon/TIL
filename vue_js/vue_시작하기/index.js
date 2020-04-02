4d04572f81d55469c2a2b9906af13e94275cb552
jihyun.kwon <jihyunkwon29@gmail.com>
2020-04-02 11:24:39 +0900

    [VUE] 사용자 입력 핸들링 : 사용자가 앱과 상호작용할수 있게 하기 위해 v-on 디렉티브를 추가한다

--- vue_js/vue_시작하기/index.js
+++ vue_js/vue_시작하기/index.js
@@ -1,14 +1,14 @@
-var app = new Vue({
-    el: "#app",
+var app_v_if = new Vue({
+    el: "#app-v-if",
     data: {
         seen: false
     }
 })
 
-app.seen = true;
+app_v_if.seen = true;
 
-var app4 = new Vue({
-    el: "#app-4",
+var app_v_for = new Vue({
+    el: "#app-v-for",
     data: {
         todos: [
             { text: 'javascript 배우기' },
@@ -18,4 +18,29 @@ var app4 = new Vue({
     }
 });
 
-app4.todos.push({ text: "New Item"});
+app_v_for.todos.push({ text: "New Item"});
+
+var app_v_on= new Vue({
+    el: "#app_v_on",
+    data: {
+        message : "Hello Reversed Vue.js!"
+    },
+    methods : {
+        reverseMessage(){
+            this.message = this.message.split('').reverse().join('');
+        }
+    }
+})
+
+var app_v_model = new Vue({
+    el: "#app-v-model",
+    data: {
+        message: "안녕하세요 Vue!"
+    }
+})
+
+Vue.component("todo-item", {
+    template: "<li>할일 항목 하나입니다.</li>"
+});
+
+var app = new Vue();
