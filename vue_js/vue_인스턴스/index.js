137424f03e8cb55d2348d56f3bf678632a785c5c
jihyun.kwon <jihyunkwon29@gmail.com>
2020-04-07 10:27:21 +0900

    [VUE-TODO] 프로젝트 초기생성

--- vue_js/vue_인스턴스/index.js
+++ vue_js/vue_인스턴스/index.js
@@ -14,3 +14,12 @@ vm.a = 2
 data.a // 2
 
 // 데이터가 변경되면 화면은 다시 렌더링됨.
+
+// 어떤 속성이 나중에 필요하다는 것을 알고 있으며, 빈 값이거나 존재하지 않은 상태로 시작한다면 아래와 같이 초기값을 지정할 필요가 있음
+data = {
+newTodoText: '',
+    visitCount: 0,
+    hideCompletedTodos: false,
+    todos: [],
+    error: null
+}
