d1dc5575e1b2a6a778fb28e15b2da91e2b9ae779
jihyun.kwon <jihyunkwon29@gmail.com>
2020-05-13 11:25:54 +0900

    [nodeJS] app.js : 비밀키 추가. port 연결 및 에러처리

--- node_js/node_bird/app.js
+++ node_js/node_bird/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
-const page = require('routes/page');
+const pageRouter = require('./routes/page');
 
 const app = express();
 
@@ -14,4 +14,35 @@ app.set('port', process.env.PORT || 8001);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false }));
+app.use(cookieParser('nodebirdsecret'));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'nodebirdsecret',
+    cookie : {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+app.use(flash());
 
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+   const err = new Error('not found');
+   err.status = 404;
+   next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
