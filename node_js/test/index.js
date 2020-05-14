76519a5882026af31c6ac02490d5610656d026f5
jihyun.kwon <jihyunkwon29@gmail.com>
2020-05-14 10:04:55 +0900

    [nodeJS] module.exports와 exports가 동일

--- node_js/test/index.js
+++ node_js/test/index.js
@@ -2,6 +2,8 @@ const {odd, even} = require('./var');
 const checkNumber = require ('./func');
 // 할일 : 모듈화한 함수와 모듈화한 변수를 이용해서 입력한 length가 짝 홀 뭔지 알아보기
 
+console.log(module.exports === exports); // module.exports와 exports는 동일하다
+
 function checkOddOrEvenLength(str){
     if(str % 2 === 0) {
         return odd;
