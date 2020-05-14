84fb85e27ec2e214c68a2b82498a671237b54eac
jihyun.kwon <jihyunkwon29@gmail.com>
2020-05-14 10:07:19 +0900

    [javascript] setTimeout, setInterval, setImmediate 정리

---
+++ node_js/test/timer.js
@@ -0,0 +1,34 @@
+const timeout = setTimeout(() => {
+    console.log("1.5초 후 실행")
+}, 1500);
+
+const interval = setInterval(() => {
+    console.log("1초마다 실행");
+}, 1000);
+
+const timeout2 = setTimeout(() => {
+    console.log("실행되지 않아요");
+}, 3000);
+
+setTimeout(() => {
+    clearTimeout(timeout2);
+    clearInterval(interval);
+}, 2500);
+
+const immediate = setImmediate(() => {
+    console.log("즉시실행");
+});
+
+const immediate2 = setImmediate(() => {
+    console.log("즉시실행되지 않아요");
+})
+
+clearImmediate(immediate2);
+
+/*
+// 결과
+즉시실행
+1초마다 실행
+1.5초 후 실행
+1초마다 실행
+*/
